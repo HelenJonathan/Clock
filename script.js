d92d91e83d339fddfae9d6ea422630fb0979bf2b
Helen Efebe <efebehelen95@gmail.com>
2022-03-07 23:21:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-setInterval(setClock, 1000);
+setInterval(setDate,1000);
 
-const hourHand = document.querySelector('[data-hour-hand]')
-const minuteHand = document.querySelector('[data-minute-hand]')
-const secondHand = document.querySelector('[data-second-hand]')
+const secHand=document.querySelector('.data-second-hand');
+const minhand=document.querySelector('.data-minute-hand');
+const hrhand=document.querySelector('.data-hour-hand');
 
-
-function setClock() {
-    const currentDate = new Date()
-    const secondsRatio = currentDate.getSeconds() / 60
-    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
-    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
-    setRotation(secondHand, secondsRatio)
-    setRotation(minuteHand, minutesRatio)
-    setRotation(hourHand, hoursRatio)
+function setDate(){
+const d = new Date();
+const hr=d.getHours();
+const m=d.getMinutes();
+const sethrdeg= 30*hr + m/2;
+hrhand.style.transform=`rotate(${sethrdeg}deg)`;
+const setmdeg=6*m;
+minhand.style.transform=`rotate(${setmdeg}deg)`;
+const s=d.getSeconds();
+const setsdeg=6*s;
+secHand.style.transform=`rotate(${setsdeg}deg)`;
 }
-
-function setRotation(element, rotationRatio) {
-    element.style.setProperty('--rotation', rotationRatio + 360)
-}
-
-setClock()
